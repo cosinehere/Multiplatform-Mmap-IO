ffc5d24653f3e8da1040652f16e09212c63572fd
cosinehere <975745601@qq.com>
2021-08-09 10:04:58 +0800

    update mio.hpp

--- MultiplatformMmapIO/Include/mio.hpp
+++ MultiplatformMmapIO/Include/mio.hpp
@@ -18,90 +18,74 @@ typedef __int64 int64_t;
 typedef unsigned __int64 uint64_t;
 #else
 #include <cstdint>
-#endif	// _MSC_VER < 1400
+#endif  // _MSC_VER < 1400
 #else
 #include <cstdint>
-#endif	// _MSC_VER
+#endif  // _MSC_VER
 
 //////////////////////////////////////////////////////////////////////////
 // system header
 //////////////////////////////////////////////////////////////////////////
-#ifdef _WIN32	// Windows
-#ifndef _AFX	// without MFC
+#if defined(_WIN32) || defined(WIN32) || defined(_WIN64)  // Windows
+#ifndef _AFX                                              // without MFC
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
-#endif	// WIN32_LEAN_AND_MEAN
+#endif  // WIN32_LEAN_AND_MEAN
 #include <windows.h>
-#endif	// _AFX
-#else	// Unix
+#endif  // _AFX
+#else   // Unix
 #include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#endif	// _WIN32
+#endif  // _WIN32
 
 //////////////////////////////////////////////////////////////////////////
 // C++11 support
 //////////////////////////////////////////////////////////////////////////
-#if __cplusplus <= 199711L && \
-    (!defined(_MSC_VER) || _MSC_VER < 1900) && \
-    (!defined(__GNUC__) || \
-    (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__ < 40603))
+#if __cplusplus <= 199711L && (!defined(_MSC_VER) || _MSC_VER < 1900) && \
+    (!defined(__GNUC__) ||                                               \
+     (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__ < 40603))
 #ifndef CXX11_NOT_SUPPORT
 #define CXX11_NOT_SUPPORT
-#endif	// CXX11_NOT_SUPPORT
-#endif	// __cplusplus<=199711L
+#endif  // CXX11_NOT_SUPPORT
+#endif  // __cplusplus<=199711L
 
 #ifdef CXX11_NOT_SUPPORT
 #define nullptr NULL
 #define constexpr const
 #define noexcept throw()
 #define override
-#endif	// CXX11_NOT_SUPPORT
+#endif  // CXX11_NOT_SUPPORT
 
-//////////////////////////////////////////////////////////////////////////
-// namespace define
-//////////////////////////////////////////////////////////////////////////
-#if !defined(NAMESPACE_BEGIN)  && !defined(NAMESPACE_END)
-#define NAMESPACE_BEGIN(name) namespace name {
-#define NAMESPACE_END }
-#endif	// !defined(NAMESPACE_BEGIN)  && !defined(NAMESPACE_END)
-
-NAMESPACE_BEGIN(mio)
+namespace mio {
 
 //////////////////////////////////////////////////////////////////////////
 // mio typedef
 //////////////////////////////////////////////////////////////////////////
-#ifdef _WIN32	// Windows
+#if defined(_WIN32) || defined(WIN32) || defined(_WIN64)  // Windows
 typedef HANDLE HMIO;
 #ifndef invalid_handle
 #define invalid_handle INVALID_HANDLE_VALUE
-#endif	// invalid_handle
-#else	// Unix
+#endif  // invalid_handle
+#define _MIO_WIN
+#else  // Unix
 #define invalid_handle -1
 typedef int HMIO;
-#endif	// _WIN32
+#endif  // _WIN32
 
 // read/write mode
-enum enum_mio_mode {
-    enum_mode_read = 0,
-    enum_mode_write
-};
+enum enum_mio_mode { enum_mode_read = 0, enum_mode_write };
 
-enum enum_mio_pos {
-    enum_pos_set = 0,
-    enum_pos_cur,
-    enum_pos_end
-};
+enum enum_mio_pos { enum_pos_set = 0, enum_pos_cur, enum_pos_end };
 
-class mio
-{
-private:
+class mio {
+   private:
     HMIO p_file;
     enum_mio_mode p_mode;
 
     bool path_empty(const char* path) { return !path || (*path == '\0'); }
 
-public:
+   public:
     mio();
     ~mio();
 #ifndef CXX11_NOT_SUPPORT
@@ -109,7 +93,7 @@ public:
     mio(const mio&&) = delete;
     mio& operator=(const mio&) = delete;
     mio& operator=(const mio&&) = delete;
-#endif	// CXX11_NOT_SUPPORT
+#endif  // CXX11_NOT_SUPPORT
 
     bool is_open() noexcept { return p_file != invalid_handle; }
 
@@ -127,211 +111,192 @@ public:
     bool file_flush();
 };
 
-inline mio::mio()
-{
-    p_file = invalid_handle;
-}
+inline mio::mio() { p_file = invalid_handle; }
 
-inline mio::~mio()
-{
-    if (is_open())
-    {
+inline mio::~mio() {
+    if (is_open()) {
         close_file();
     }
 }
 
-inline bool mio::create_file(const char* path, enum_mio_mode mode)
-{
-    if (path_empty(path))
-    {
+inline bool mio::create_file(const char* path, enum_mio_mode mode) {
+    if (path_empty(path)) {
         return false;
     }
 
     HMIO handle = invalid_handle;
-#ifdef _WIN32
-    handle = CreateFile(path,
+#ifdef _MIO_WIN
+    handle = CreateFile(
+        path,
         (mode == enum_mode_read) ? GENERIC_READ : GENERIC_READ | GENERIC_WRITE,
-        FILE_SHARE_READ | FILE_SHARE_WRITE,
-        0,
-        CREATE_ALWAYS,
-        FILE_ATTRIBUTE_NORMAL,
-        0);
+        FILE_SHARE_READ | FILE_SHARE_WRITE, 0, CREATE_ALWAYS,
+        FILE_ATTRIBUTE_NORMAL, 0);
 #else
-    handle = open(path,
-        (mode == enum_mode_read) ? O_RDONLY | O_CREAT : O_RDWR | O_CREAT,
+    handle = open(
+        path, (mode == enum_mode_read) ? O_RDONLY | O_CREAT : O_RDWR | O_CREAT,
         S_IRUSR | S_IWUSR);
-#endif	// _WIN32
+#endif  // _MIO_WIN
 
     p_file = handle;
     p_mode = mode;
     return handle != invalid_handle;
 }
 
-inline bool mio::open_file(const char* path, enum_mio_mode mode)
-{
-    if (path_empty(path))
-    {
+inline bool mio::open_file(const char* path, enum_mio_mode mode) {
+    if (path_empty(path)) {
         return false;
     }
 
     HMIO handle = invalid_handle;
-#ifdef _WIN32
-    handle = CreateFile(path,
+#ifdef _MIO_WIN
+    handle = CreateFile(
+        path,
         (mode == enum_mode_read) ? GENERIC_READ : GENERIC_READ | GENERIC_WRITE,
-        FILE_SHARE_READ | FILE_SHARE_WRITE,
-        0,
-        OPEN_EXISTING,
-        FILE_ATTRIBUTE_NORMAL,
-        0);
+        FILE_SHARE_READ | FILE_SHARE_WRITE, 0, OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL, 0);
 #else
-    handle = open(path,
-        (mode == enum_mode_read) ? O_RDONLY : O_RDWR);
-#endif	// _WIN32
+    handle = open(path, (mode == enum_mode_read) ? O_RDONLY : O_RDWR);
+#endif  // _WIN32
 
     p_file = handle;
     p_mode = mode;
     return handle != invalid_handle;
 }
 
-inline void mio::close_file()
-{
-    if (!is_open())
-    {
+inline void mio::close_file() {
+    if (!is_open()) {
         return;
     }
 
-#ifdef _WIN32
+#ifdef _MIO_WIN
     CloseHandle(p_file);
 #else
     close(p_file);
-#endif	// _WIN32
+#endif  // _MIO_WIN
 
     p_file = invalid_handle;
 }
 
-inline size_t mio::file_size()
-{
-    if (!is_open())
-    {
+inline size_t mio::file_size() {
+    if (!is_open()) {
         return 0;
     }
 
-#ifdef _WIN32
+#ifdef _MIO_WIN
     LARGE_INTEGER size;
-    if (GetFileSizeEx(p_file, &size) == FALSE)
-    {
+    if (GetFileSizeEx(p_file, &size) == FALSE) {
         return 0;
     }
     return static_cast<size_t>(size.QuadPart);
 #else
     struct stat st;
-    if (fstat(p_file, &st) == -1)
-    {
+    if (fstat(p_file, &st) == -1) {
         return 0;
     }
     return static_cast<size_t>(st.st_size);
-#endif
+#endif  // _MIO_WIN
 }
 
-inline size_t mio::read_file(void* buffer, size_t readnum)
-{
-    if (!is_open())
-    {
+inline size_t mio::read_file(void* buffer, size_t readnum) {
+    if (!is_open()) {
         return 0;
     }
 
-    if (p_mode != enum_mode_read)
-    {
+    if (p_mode != enum_mode_read) {
         return 0;
     }
 
-#ifdef _WIN32
+#ifdef _MIO_WIN
     DWORD dwRead = 0;
-    if (!ReadFile(p_file, buffer, readnum, &dwRead, nullptr))
-    {
+    if (!ReadFile(p_file, buffer, readnum, &dwRead, nullptr)) {
         return 0;
     }
     return dwRead;
 #else
     return read(p_file, buffer, readnum);
-#endif	// _WIN32
+#endif  // _MIO_WIN
 }
 
-inline size_t mio::write_file(const void* buffer, size_t writenum)
-{
-    if (!is_open())
-    {
+inline size_t mio::write_file(const void* buffer, size_t writenum) {
+    if (!is_open()) {
         return 0;
     }
 
-    if (p_mode != enum_mode_write)
-    {
+    if (p_mode != enum_mode_write) {
         return 0;
     }
 
-#ifdef _WIN32
+#ifdef _MIO_WIN
     DWORD dwWrite = 0;
-    if (!WriteFile(p_file, buffer, writenum, &dwWrite, nullptr))
-    {
+    if (!WriteFile(p_file, buffer, writenum, &dwWrite, nullptr)) {
         return 0;
     }
     return dwWrite;
 #else
     return write(p_file, buffer, writenum);
-#endif	// _WIN32
+#endif  // _MIO_WIN
 }
 
-inline off_t mio::seek_file(enum_mio_pos pos, off_t offset)
-{
-    if (!is_open())
-    {
+inline off_t mio::seek_file(enum_mio_pos pos, off_t offset) {
+    if (!is_open()) {
         return -1;
     }
 
-#ifdef _WIN32
+#ifdef _MIO_WIN
     LONG newoff;
     DWORD move;
-    switch (pos)
-    {
-    case enum_pos_set: move = FILE_BEGIN; break;
-    case enum_pos_cur: move = FILE_CURRENT; break;
-    case enum_pos_end: move = FILE_END; break;
-    default: move = FILE_CURRENT; break;
+    switch (pos) {
+        case enum_pos_set:
+            move = FILE_BEGIN;
+            break;
+        case enum_pos_cur:
+            move = FILE_CURRENT;
+            break;
+        case enum_pos_end:
+            move = FILE_END;
+            break;
+        default:
+            move = FILE_CURRENT;
+            break;
     }
-    if (SetFilePointer(p_file, offset, &newoff, move) == FALSE)
-    {
+    if (SetFilePointer(p_file, offset, &newoff, move) == FALSE) {
         return -1;
     }
 
     return static_cast<size_t>(newoff);
 #else
     int move;
-    switch (pos)
-    {
-    case enum_pos_set: move = SEEK_SET; break;
-    case enum_pos_cur: move = SEEK_CUR; break;
-    case enum_pos_end: move = SEEK_END; break;
-    default: move = SEEK_CUR; break;
+    switch (pos) {
+        case enum_pos_set:
+            move = SEEK_SET;
+            break;
+        case enum_pos_cur:
+            move = SEEK_CUR;
+            break;
+        case enum_pos_end:
+            move = SEEK_END;
+            break;
+        default:
+            move = SEEK_CUR;
+            break;
     }
     off_t newoff = lseek(p_file, offset, move);
     return newoff;
-#endif	// _WIN32
+#endif  // _MIO_WIN
 }
 
-inline bool mio::file_flush()
-{
-    if (!is_open() || p_mode != enum_mode_write)
-    {
+inline bool mio::file_flush() {
+    if (!is_open() || p_mode != enum_mode_write) {
         return false;
     }
 
-#ifdef _WIN32
+#ifdef _MIO_WIN
     return (FlushFileBuffers(p_file) == TRUE);
 #else
     return (fsync(p_file) == 0);
-#endif
+#endif  // _MIO_WIN
 }
 
-NAMESPACE_END
+}  // namespace mio
 
-#endif	// _MIO_HPP_
+#endif  // _MIO_HPP_
